f15863afbe426647d937881a32d3939afd88b63a
Salvador Guzman <guzmansalv@gmail.com>
2022-02-05 16:41:08 -0800

    made progress with util funcs

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 import atexit
+import json
 import logging
+import os
 import requests
+import typing
+from typing import Dict
+from typing import Optional
 
 
 class Init:
+    json_path: str = './.env.json'
+    data: Optional[Dict] = None
+
     @staticmethod
     def __logging() -> None:
         logging.basicConfig(
@@ -20,10 +28,31 @@ class Init:
         atexit.register(lambda: logging.info('hi'))
 
     @staticmethod
+    def __json() -> None:
+        path: str = Init.json_path
+        logging.info('Loaded %s', path)
+
+        fs = open(path, 'r')
+
+        obj: Dict = json.load(fs)
+        Init.data = obj
+
+    @staticmethod
     def exec() -> None:
         Init.__logging()
         Init.__greet()
         Init.__exit()
+        Init.__json()
+
+
+class Util:
+    @staticmethod
+    def __pretty_print(d: Dict) -> str:
+        return json.dumps(d, indent=2)
+
+    @staticmethod
+    def data() -> None:
+        print(Util.__pretty_print(Init.data))
 
 
 def main() -> None:
