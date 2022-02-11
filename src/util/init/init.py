5de9b25c9518d78803d35226dda82416f9ab3f61
Salvador Guzman <guzmansalv@gmail.com>
2022-02-11 14:04:19 -0800

    make modules private by default

--- src/util/init/init.py
+++ src/util/init/init.py
@@ -1,11 +1,11 @@
-from . import log
-from . import exit
-from . import greet
-from . import json
+from . import __log as log
+from . import __exit as exit
+from . import __greet as greet
+from . import __json as json
 
 
 def exec() -> None:
     log.exec()
     greet.exec()
     exit.exec()
-    json.exec()
+    json.json()
